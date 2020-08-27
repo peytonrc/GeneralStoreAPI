bd94fa50cfbb936ce23d9a4918ff96b7b94aa45a
peytonrc <peytnrose@gmail.com>
2020-08-27 12:02:12 -0400

    commit

---
+++ GeneralStoreAPIProject/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using GeneralStoreAPIProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace GeneralStoreAPIProject.Controllers // Controllers: Remote for TV; holds all the logic to display whatever the commad that is given. 
+{
+    public class ProductController : ApiController
+    {
+        private StoreDbContext _context = new StoreDbContext();
+
+        //Post
+
+        //Get 
+
+        //Get{id}
+
+        //Put
+
+        //Delete
+    }
+}
